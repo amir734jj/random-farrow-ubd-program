62a0daa33ea8096ac354a686d5c3d77c229c52c3
Amir Hesamian <hesamian@uwm.edu>
2026-06-02 17:34:16 +0000

    remove unnessary scripts

--- gen_programs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p programs
-for n in $(seq 10 10 1000); do
-    if [[ ! -f "programs/${n}.program" ]]; then
-        python3 get_farrow.py --depth="$n" > "programs/${n}.program"
-    fi
-done
